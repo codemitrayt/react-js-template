e6cda8ce385d6142be381ae4fe84949ca7993e1c
codemitrayt <codemitrayt@gmail.com>
2026-01-12 20:44:12 +0530

    feat: add get started button

--- src/components/shared/header.jsx
+++ src/components/shared/header.jsx
@@ -1,15 +1,21 @@
+import { useNavigate } from 'react-router'
+
 import { Logo, ThemeToggle } from '@/components'
 import { Button } from '@/components/ui/button'
 
 const Header = () => {
+  const navigate = useNavigate()
   return (
     <nav className="h-14 border-b flex items-center">
       <div className="cotainer mx-auto w-full px-4 flex items-center justify-between">
         <Logo />
         <div className="flex gap-2 items-center justify-center">
-          <Button size="sm">Sign In</Button>
-          <Button size="sm" variant="outline">
-            Sign Up
+          <Button
+            onClick={() => navigate('/auth/sign-in')}
+            variant="outline"
+            size="sm"
+          >
+            Get Started
           </Button>
           <ThemeToggle />
         </div>
